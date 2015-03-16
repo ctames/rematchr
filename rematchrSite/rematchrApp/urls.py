ae0605f95b12883d469d0225dc622d43acafaf15
ctames <cta5bb@virginia.edu>
2015-03-16 13:35:28 -0400

    reverted back to starting point for views and urls, taking a different approach

--- rematchrSite/rematchrApp/urls.py
+++ rematchrSite/rematchrApp/urls.py
@@ -5,11 +5,7 @@ from rematchrApp import views
 
 urlpatterns = patterns('', 
 	url(r'^$', views.index, name='index'),
-	url(r'^conferences/(?P<conference_id>\d+)/$', views.conference, name='conference'),
-	url(r'^conferences/(?P<conference_id>\d+)/researchers/(?P<researcher_id>\d+)/$',
-		views.researcher, name='researcher'),
-	url(r'^conferences/(?P<conference_id>\d+)/reviewers/(?P<reviewer_id>\d+)/$',
-		views.reviewer, name='reviewer'),
+	url(r'^', views.index),
 )
 
 
--- rematchrSite/rematchrApp/views.py
+++ rematchrSite/rematchrApp/views.py
@@ -5,24 +5,7 @@ from django.http import HttpResponse
 from rematchrApp.models import Conference, Researcher, Reviewer
 
 def index(request):
-	conference_list = Conference.objects.order_by('id')
-	context = {'conference_list': conference_list}
-	return render(request, 'rematchrApp/index.html', context)	
+	return render(request, 'rematchrApp/index.html')
 
-def conference(request, conference_id):
-	conference = get_object_or_404(Conference, pk=conference_id)
-	context = {'conference': conference}
-	return render(request, 'rematchrApp/conference.html', context) 
 
-def researcher(request, conference_id, researcher_id):	
-	conference = get_object_or_404(Conference, pk=conference_id)
-	researcher = get_object_or_404(Researcher, pk=researcher_id)
-	context = {'conference': conference, 'researcher': researcher}
-	return render(request, 'rematchrApp/researcher.html', context)
-
-def reviewer(request, conference_id, reviewer_id):
-	conference = get_object_or_404(Conference, pk=conference_id)
-	reviewer = get_object_or_404(Reviewer, pk=reviewer_id)
-	context = {'conference': conference, 'reviewer': reviewer}
-	return render(request, 'rematchrApp/reviewer.html', context)
 
