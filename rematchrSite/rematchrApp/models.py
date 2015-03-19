8ea5be328bdd43d5511da18b71a8810e9501add9
ctames <cta5bb@virginia.edu>
2015-03-19 13:00:43 -0400

    some changes to models

--- rematchrSite/rematchrApp/models.py
+++ rematchrSite/rematchrApp/models.py
@@ -1,19 +1,21 @@
 # rematchrApp/models.py
 
+from django.contrib.auth.models import User
 from django.db import models
 import json
 
 class Conference(models.Model):
-	title = models.CharField(max_length=100)
-	date = models.DateTimeField()	 
-	
+	title = models.CharField(max_length=256)
+	date = models.DateTimeField()
+	user = models.ForeignKey(User, unique=True)	 
+		
 	def __unicode__(self):
 		return '%s' % self.title
 
 class Researcher(models.Model):
-	firstname = models.CharField(max_length=50)
-	lastname = models.CharField(max_length=50)
 	conference = models.ForeignKey(Conference)
+	firstname = models.CharField(max_length=256)
+	lastname = models.CharField(max_length=256)
 	doc_texts = models.TextField(blank=True)
 	doc_urls = models.TextField(blank=True)
 	
@@ -34,8 +36,22 @@ class Researcher(models.Model):
 
 class Reviewer(models.Model):	
 	conference = models.ForeignKey(Conference)
-	firstname = models.CharField(max_length=50)
-	lastname = models.CharField(max_length=50)	
+	firstname = models.CharField(max_length=256)
+	lastname = models.CharField(max_length=256)	
+	doc_texts = models.TextField(blank=True)
+	doc_urls = models.TextField(blank=True)
+	
+	def set_doc_texts(self, texts):
+		self.doc_texts = json.dump(texts)
+
+	def get_doc_texts(self):
+		return json.loads(self.doc_texts)	
+
+	def set_doc_urls(self, urls):
+		self.doc_urls = json.dump(urls)
+
+	def get_doc_urls(self):
+		return json.loads(self.doc_urls)	
 
 	def __unicode__(self):
 		return '%s, %s : %s' % (self.lastname, self.firstname, self.conference)
