691efe90b063766fdd22345da208c177faaf8ae8
ctames <cta5bb@virginia.edu>
2015-03-25 17:35:46 -0400

    list of conferences on account page, edit page, correctly linked, can't view other user's conferences

--- rematchrSite/rematchrApp/urls.py
+++ rematchrSite/rematchrApp/urls.py
@@ -5,7 +5,7 @@ from rematchrApp import views
 
 urlpatterns = patterns('', 
 	url(r'^$', views.index, name='index'),	
-	url(r'^account/editconf/', views.conferenceEdit, name='editconf'),
+	url(r'^account/conference/(?P<conference_id>\d+)/', views.conferenceEdit, name='editconf'),
 	url(r'^account/newconf/', views.conferenceAdd, name='newconf'),
 	url(r'^account', views.account, name='account'),
 	url(r'^about', views.about, name='about'),
--- rematchrSite/rematchrApp/views.py
+++ rematchrSite/rematchrApp/views.py
@@ -4,22 +4,32 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from rematchrApp.models import Conference, Researcher, Reviewer
 from rematchrApp.forms import ConferenceForm
+from django.contrib.auth.models import User
 
 def index(request):
 	return render(request, 'rematchrApp/index.html')
 
-def account(request):
-	return render(request, 'rematchrApp/account.html')
+def account(request):	
+	conferences = None
+	if request.user.is_authenticated():
+		conferences = request.user.conference_set.all()
+	context = { 'conferences': conferences, }
+	return render(request, 'rematchrApp/account.html', context)
 
-def about(request):
+def about(request):	
 	return render(request, 'rematchrApp/about.html')
 
 def conferenceAdd(request):
+	id = 0
+	try:
+		id = Conference.objects.latest('id').id + 1
+	except Conference.DoesNotExist:
+		pass
 	form = ConferenceForm()
-	context = { "conferenceForm" : form, }	
+	context = { "conferenceForm" : form, 'id' : id, }	
 	return render(request, 'rematchrApp/create_conference.html', context)
 
-def conferenceEdit(request):
+def conferenceEdit(request, conference_id):
 	conference = None
 	if request.method == 'POST':
 		form = ConferenceForm(request.POST)
@@ -27,5 +37,10 @@ def conferenceEdit(request):
 		if request.user.is_authenticated():
 			conference.user = request.user
 		conference.save()
+	else:
+		conference = Conference.objects.get(id=conference_id)
+		if request.user.is_authenticated():
+			if conference.user != request.user:
+				return render(request, 'rematchrApp/invalid_user.html')
 	context = { "conference" : conference, }
 	return render(request, 'rematchrApp/edit_conference.html', context)
